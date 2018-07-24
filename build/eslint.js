c79b4886e2ec42c23802645289c27bca5f72d3ef
awjh-ibm <andrew.hurt1@ibm.com>
2018-07-24 09:56:53 +0100

    moving eslint config to file
    Change-Id: Id6754926cf07e173e30c53c664280e592728b817
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

--- build/eslint.js
+++ build/eslint.js
@@ -16,32 +16,5 @@ gulp.task('lint', function () {
 		'!docs/**'
 	], {
 		base: path.join(__dirname, '..')
-	}).pipe(eslint(
-		{
-			env: ['es6', 'node'],
-			extends: 'eslint:recommended',
-			parserOptions: {
-				sourceType: 'module',
-				ecmaVersion: 8,
-			},
-			rules: {
-				indent: ['error', 'tab'],
-				'linebreak-style': ['error', 'unix'],
-				quotes: ['error', 'single'],
-				semi: ['error', 'always'],
-				'no-trailing-spaces': ['error'],
-				'max-len': [
-					'error',
-					{
-						'code': 150,
-						'ignoreTrailingComments': true,
-						'ignoreUrls': true,
-						'ignoreStrings': true,
-						'ignoreTemplateLiterals': true,
-						'ignoreRegExpLiterals': true
-					}
-				]
-			}
-		}
-	)).pipe(eslint.format()).pipe(eslint.failAfterError());
+	}).pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError());
 });
