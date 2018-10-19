98c325d5acd1565a47ec4e5750c0299496d6d7fe
zhaochy <zhaochy_2015@hotmail.com>
2018-10-19 10:41:50 +0800

    [FAB-12537] CC convenience layer for state-based EP
    This implementation of the CC convenience layer interface facilitates
    the usage of state-based endorsement. It allows easy modification of
    a state-based endorsement policy providing Add/Delete functions that
    are based on MSP-IDs and automatically creates the underlying
    endorsement policy.
    
    Change-Id: Ibfc9bf8a00d3c53ca50efe7cf4574c56b81e4d54
    Signed-off-by: zhaochy <zhaochy_2015@hotmail.com>

---
+++ fabric-shim/lib/utils/statebased.js
@@ -0,0 +1,155 @@
+/*
+# Copyright Zhao Chaoyi. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+const grpc = require('grpc');
+const path = require('path');
+
+const _policiesProto = grpc.load({
+    root: path.join(__dirname, '../protos'),
+    file: 'common/policies.proto'
+}).common;
+
+const _principalProto = grpc.load({
+    root: path.join(__dirname, '../protos'),
+    file: 'msp/msp_principal.proto'
+}).common;
+
+const ROLE_TYPE_MEMBER = 'MEMBER';
+const ROLE_TYPE_PEER = 'PEER';
+
+
+/**
+ * KeyEndorsementPolicy is used to help set endorsement policies and decode them
+ * into validation parameter byte arrays, the shim provides convenience functions
+ * that allow the chaincode developer to deal with endorsement policies in terms
+ * of the MSP identifiers of organizations.
+ * For more informations, please read the [documents]{@link https://hyperledger-fabric.readthedocs.io/en/latest/endorsement-policies.html#setting-key-level-endorsement-policies}
+ *
+ * @class
+ */
+class KeyEndorsementPolicy {
+    /**
+     * The construcotr accepts an optional argument `policy`.
+     * The argument `policy` will be parsed if provided.
+     * @param {Buffer} policy the endorsement policy retrieved for a key
+     */
+    constructor(policy) {
+        this.orgs = {};
+        if (policy) {
+            const spe = _policiesProto.SignaturePolicyEnvelope.decode(policy);
+            this._setMspIdsFromSPE(spe);
+        }
+    }
+
+    /**
+     * returns the endorsement policy as bytes
+     * @returns {Buffer} the endorsement policy
+     */
+    getPolicy() {
+        const spe = this._getPolicyFromMspId();
+        return spe.toBuffer();
+    }
+
+    /**
+     * adds the specified orgs to the list of orgs that are required
+     * to endorse
+     * @param {string} role the role of the new org(s). i.e., MEMBER or PEER
+     * @param  {...string} neworgs the new org(s) to be added to the endorsement policy
+     */
+    addOrgs(role, ...neworgs) {
+        let mspRole;
+        switch (role) {
+            case ROLE_TYPE_MEMBER:
+                mspRole = _principalProto.MSPRole.MSPRoleType.MEMBER;
+                break;
+            case ROLE_TYPE_PEER:
+                mspRole = _principalProto.MSPRole.MSPRoleType.PEER;
+                break;
+            default:
+                throw new Error(`role type ${role} does not exist`);
+        }
+
+        neworgs.forEach(neworg => {
+            this.orgs[neworg] = mspRole;
+        });
+    }
+
+    /**
+     * delete the specified channel orgs from the existing key-level endorsement
+     * policy for this KVS key.
+     * @param  {...string} delorgs the org(s) to be deleted
+     */
+    delOrgs(...delorgs) {
+        delorgs.forEach(delorg => {
+            delete this.orgs[delorg];
+        });
+    }
+
+    /**
+     * listOrgs returns an array of channel orgs that are required to endorse changes
+     * @return {string[]} the list of channel orgs that are required to endorse changes
+     */
+    listOrgs() {
+        return Object.keys(this.orgs);
+    }
+
+    /**
+     * Internal used only, set the orgs map from a signature policy envelope
+     * @param {_policiesProto.SignaturePolicyEnvelope} signaturePolicyEnvelope the signaturePolicyEnvelope
+     *  decoded from the endorsement policy.
+     */
+    _setMspIdsFromSPE(signaturePolicyEnvelope) {
+        // iterate over the identities in this envelope
+        signaturePolicyEnvelope.identities.forEach(identity => {
+            // this imlementation only supports the ROLE type
+            /* istanbul ignore else */
+            if (identity.PrincipalClassification === _principalProto.MSPPrincipal.ROLE) {
+                const msprole = _principalProto.MSPRole.decode(identity.principal);
+                this.orgs[msprole.msp_identifier] = msprole.role;
+            }
+        });
+    }
+
+    /**
+     * Internal used only. construct the policy from all orgs' mspIds.
+     * the policy requires exactly 1 signature from all of the principals.
+     * @returns {_policiesProto.SignaturePolicyEnvelope} return the SignaturePolicyEnvelope instance
+     */
+    _getPolicyFromMspId() {
+        const mspIds = this.listOrgs();
+        const principals = [];
+        const sigsPolicies = [];
+        mspIds.forEach((mspId, i) => {
+            const mspRole = new _principalProto.MSPRole();
+            mspRole.setRole(this.orgs[mspId]);
+            mspRole.setMspIdentifier(mspId);
+            const principal = new _principalProto.MSPPrincipal();
+            principal.setPrincipalClassification(_principalProto.MSPPrincipal.Classification.ROLE);
+            principal.setPrincipal(mspRole.toBuffer());
+            principals.push(principal);
+
+            const signedBy = new _policiesProto.SignaturePolicy();
+            signedBy.set('signed_by', i);
+            sigsPolicies.push(signedBy);
+        });
+
+        // create the policy: it requires exactly 1 signature from all of the principals
+        const allOf = new _policiesProto.SignaturePolicy.NOutOf();
+        allOf.setN(mspIds.length);
+        allOf.setRules(sigsPolicies);
+
+        const noutof = new _policiesProto.SignaturePolicy();
+        noutof.set('n_out_of', allOf);
+
+        const spe = new _policiesProto.SignaturePolicyEnvelope();
+        spe.setVersion(0);
+        spe.setRule(noutof);
+        spe.setIdentities(principals);
+        return spe;
+    }
+}
+
+module.exports.KeyEndorsementPolicy = KeyEndorsementPolicy;
---
+++ fabric-shim/test/unit/utils/statebased.js
@@ -0,0 +1,169 @@
+/*
+# Copyright Zhao Chaoyi. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+
+
+// const rewire = require('rewire');
+
+const chai = require('chai');
+const expect = chai.expect;
+const {KeyEndorsementPolicy} = require('../../../lib/utils/statebased');
+
+describe('KeyEndorsementPolicy', () => {
+    describe('#constructor', () => {
+        it('should success for no policy', () => {
+            expect(() => new KeyEndorsementPolicy()).not.throw;
+        });
+
+        it('should decode policy', () => {
+            const ep = new KeyEndorsementPolicy();
+            ep.addOrgs('MEMBER', 'Org1MSP', 'Org2MSP');
+            const policy = ep.getPolicy();
+            const anotherEp = new KeyEndorsementPolicy(policy);
+            expect(anotherEp.orgs).to.deep.eql({
+                Org1MSP: 0,
+                Org2MSP: 0
+            });
+        });
+    });
+
+    describe('#addOrgs', () => {
+        let ep;
+
+        beforeEach(() => {
+            ep = new KeyEndorsementPolicy();
+        });
+
+        it('should throw error if role is not supported', () => {
+            expect(() => ep.addOrgs('aDummyRole', 'org1msp')).to.throw(/role type aDummyRole does not exist/);
+        });
+
+        it('should throw error if role is missing', () => {
+            expect(() => ep.addOrgs()).to.throw(/role type undefined does not exist/);
+        });
+
+        it('should success add multiple orgs', () => {
+            ep.addOrgs('MEMBER', 'Org1MSP', 'Org2MSP');
+            expect(ep).haveOwnProperty('orgs').not.empty;
+            expect(ep.orgs).haveOwnProperty('Org1MSP').eqls(0);
+            expect(ep.orgs).haveOwnProperty('Org2MSP').eqls(0);
+            expect(Object.keys(ep.orgs).length).to.eql(2);
+        });
+
+        it('should success add one org', () => {
+            ep.addOrgs('MEMBER', 'Org1MSP');
+            expect(ep).haveOwnProperty('orgs').not.empty;
+            expect(ep.orgs).haveOwnProperty('Org1MSP').eqls(0);
+            expect(Object.keys(ep.orgs).length).to.eql(1);
+        });
+
+        it('should success add one peer org', () => {
+            ep.addOrgs('PEER', 'Org1MSP');
+            expect(ep).haveOwnProperty('orgs').not.empty;
+            expect(ep.orgs).haveOwnProperty('Org1MSP').eqls(3);
+            expect(Object.keys(ep.orgs).length).to.eql(1);
+        });
+
+        it('should not throw error if no orgs', () => {
+            ep.addOrgs('MEMBER');
+            expect(ep).haveOwnProperty('orgs').to.be.empty;
+        });
+    });
+
+    describe('#_getPolicyFromMspId', () => {
+        let ep;
+        beforeEach(() => {
+            ep = new KeyEndorsementPolicy();
+            ep.addOrgs('MEMBER', 'Org1MSP', 'Org2MSP', 'Org3MSP');
+        });
+
+        it('should successfully get policy from mspIds', () => {
+            const policy = ep._getPolicyFromMspId();
+            expect(policy).haveOwnProperty('version').eqls(0);
+            expect(policy).haveOwnProperty('identities').to.be.an('array');
+            expect(policy.identities.length).to.eql(3);
+            expect(policy.rule.toRaw()).to.deep.eqls({
+                n_out_of: {
+                    n: 3,
+                    rules: [{
+                        n_out_of: null,
+                        signed_by: 0,
+                        Type: 'signed_by'
+                    }, {
+                        n_out_of: null,
+                        signed_by: 1,
+                        Type: 'signed_by'
+                    }, {
+                        n_out_of: null,
+                        signed_by: 2,
+                        Type: 'signed_by'
+                    }]
+                },
+                signed_by: 0,
+                Type: 'n_out_of'
+            });
+        });
+    });
+
+    describe('#_setMspIdsFromSPE', () => {
+        let spe;
+        beforeEach(() => {
+            const ep = new KeyEndorsementPolicy();
+            ep.addOrgs('MEMBER', 'Org1MSP', 'Org2MSP', 'Org3MSP');
+            ep.addOrgs('PEER', 'Org4MSP', 'Org5MSP');
+            spe = ep._getPolicyFromMspId();
+        });
+
+        it('should successfully set mspIds from spe', () => {
+            const ep = new KeyEndorsementPolicy();
+            ep._setMspIdsFromSPE(spe);
+            expect(ep.orgs).haveOwnProperty('Org1MSP').eqls(0);
+            expect(ep.orgs).haveOwnProperty('Org2MSP').eqls(0);
+            expect(ep.orgs).haveOwnProperty('Org3MSP').eqls(0);
+            expect(ep.orgs).haveOwnProperty('Org4MSP').eqls(3);
+            expect(ep.orgs).haveOwnProperty('Org5MSP').eqls(3);
+        });
+    });
+
+    describe('#delOrgs', () => {
+        let ep;
+
+        beforeEach(() => {
+            ep = new KeyEndorsementPolicy();
+            ep.addOrgs('MEMBER', 'Org1MSP', 'Org2MSP', 'Org3MSP');
+        });
+
+        it('should successfully delete one org', () => {
+            ep.delOrgs('Org1MSP');
+            expect(ep.orgs).haveOwnProperty('Org2MSP').eqls(0);
+            expect(ep.orgs).haveOwnProperty('Org3MSP').eqls(0);
+            expect(ep.orgs).does.not.haveOwnProperty('Org1MSP');
+        });
+
+        it('should successfully delete multiple orgs', () => {
+            ep.delOrgs('Org1MSP', 'Org3MSP');
+            expect(ep.orgs).haveOwnProperty('Org2MSP').eqls(0);
+            expect(ep.orgs).does.not.haveOwnProperty('Org1MSP');
+            expect(ep.orgs).does.not.haveOwnProperty('Org3MSP');
+        });
+    });
+
+    describe('#getPolicy', () => {
+        let ep;
+
+        beforeEach(() => {
+            ep = new KeyEndorsementPolicy();
+            ep.addOrgs('MEMBER', 'Org1MSP', 'Org2MSP', 'Org3MSP');
+        });
+
+        it('should successfully get policy', () => {
+            const policy = ep.getPolicy();
+            const anotherEp = new KeyEndorsementPolicy(policy);
+            expect(anotherEp.orgs).to.haveOwnProperty('Org1MSP');
+            expect(anotherEp.orgs).to.haveOwnProperty('Org2MSP');
+            expect(anotherEp.orgs).to.haveOwnProperty('Org3MSP');
+        });
+    });
+});
