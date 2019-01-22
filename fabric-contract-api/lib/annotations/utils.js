26e6fb62ffa4c52f1fcc88078a928d14a3dcffff
awjh-ibm <andrew.hurt1@ibm.com>
2019-01-22 16:47:51 +0000

    [FAB-12846] added ability to specify arrays
    Change-Id: I9082ecbacd72017836e1902ec6def09c0ef1f53a
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

--- fabric-contract-api/lib/annotations/utils.js
+++ fabric-contract-api/lib/annotations/utils.js
@@ -12,6 +12,8 @@
  * limitations under the License.
  */
 
+const refPath = '#/components/schemas/';
+
 module.exports.appendOrUpdate = function appendOrUpdate(arr, primaryField, id, data) {
     const objExists = arr.some((obj) => {
         if (obj[primaryField] === id) {
@@ -26,3 +28,73 @@ module.exports.appendOrUpdate = function appendOrUpdate(arr, primaryField, id, d
         arr.push(obj);
     }
 };
+
+module.exports.findByValue = function findByValue(arr, primaryField, id) {
+    for (const el of arr) {
+        if (el[primaryField] === id) {
+            return el;
+        }
+    }
+
+    return null;
+};
+
+const generateSchema = (type) => {
+    if (isPrimitive(type)) {
+        return {
+            type: type.toLowerCase()
+        };
+    } else if (isArray(type)) {
+        const subType = getSubArray(type);
+
+        return  {
+            type: 'array',
+            items: generateSchema(subType)
+        };
+    }
+
+    return {
+        $ref: refPath + type
+    };
+};
+module.exports.generateSchema = generateSchema;
+
+// there appears to be confusions within the meta data handling
+// whether string or String is correct.
+// string is the preferred for JSON Schema
+function isPrimitive(type) {
+    const lowerCase = type.toLowerCase();
+    switch (lowerCase) {
+        case 'string':
+        case 'number':
+        case 'boolean':
+            return lowerCase;
+
+        default:
+            return undefined;
+    }
+}
+
+// Like Array<number>
+function isArrowedArray(type) {
+    return /^Array<[A-z].*>$/.test(type);
+}
+
+// Like number[]
+function isBracketArray(type) {
+    return /^[A-z].*(\[\])+?/.test(type);
+}
+
+// determine if string representation of type
+// is in format of an arrray descriptor
+function isArray(type) {
+    return isArrowedArray(type) || isBracketArray(type);
+}
+
+function getSubArray(type) {
+    if (isArrowedArray(type)) {
+        return type.replace('Array<', '').replace('>', '');
+    }
+
+    return type.replace('[]', '');
+}
