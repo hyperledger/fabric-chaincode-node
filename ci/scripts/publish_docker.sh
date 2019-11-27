d4c42360bcf332e7307a8a0267fb98f1cbefe1c0
Matthew B. White <whitemat@uk.ibm.com>
2019-11-27 10:44:56 +0000

    Fix typo in docker login cmd
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>

--- ci/scripts/publish_docker.sh
+++ ci/scripts/publish_docker.sh
@@ -7,6 +7,6 @@ set -euo pipefail
 docker image load --input build/fabric-nodeenv.tar.gz         # gets the build image of nodeenv
 docker images
 
-docker login nexus3.hyperledger.org:10003 --username="${NEXUS_USERNAME}" --pasword="${NEXUS_PASSWORD}"
+docker login nexus3.hyperledger.org:10003 --username="${NEXUS_USERNAME}" --password="${NEXUS_PASSWORD}"
 docker tag hyperledger/fabric-nodeenv "${NEXUS_URL}/fabric-nodeenv:amd64-${MAPPED_VERSION}-stable"
 docker push "${NEXUS_URL}/fabric-nodeenv:amd64-${MAPPED_VERSION}-stable"
