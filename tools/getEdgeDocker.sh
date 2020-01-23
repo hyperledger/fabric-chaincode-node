9bcd0b3734ff38e2e496e4a134eb1ae34aca9189
James Taylor <jamest@uk.ibm.com>
2020-01-23 11:18:49 +0000

    [FABCN-377] Prepare for v2.1 unstable builds (WIP)
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- tools/getEdgeDocker.sh
+++ tools/getEdgeDocker.sh
@@ -11,25 +11,32 @@ ARTIFACTORY_URL=hyperledger-fabric.jfrog.io
 ORG_NAME="hyperledger"
 
 VERSION=2.0.0
+CA_VERSION=1.4.4
 ARCH="amd64"
-: ${STABLE_VERSION:=$VERSION-stable}
-STABLE_TAG=$ARCH-$STABLE_VERSION
 MASTER_TAG=$ARCH-master
-
-echo "---------> STABLE_VERSION:" $STABLE_VERSION
 
 # Specifically not pulling down javaenv nodeenv
 # javaenv not needed and we've just rebuilt the nodeenv
 
 dockerTag() {
-  for IMAGES in peer orderer ca ca orderer baseos ccenv  tools; do
+  for IMAGES in peer orderer ca baseos ccenv tools; do
     echo "Images: $IMAGES"
     echo
+
+    if [ "${IMAGES}" = "ca" ]; then
+      STABLE_TAG=$ARCH-$CA_VERSION-stable
+    else
+      STABLE_TAG=$ARCH-$VERSION-stable
+    fi
+    echo "---------> STABLE_TAG:" $STABLE_TAG
+    echo
+
     docker pull $ARTIFACTORY_URL/fabric-$IMAGES:$STABLE_TAG
-          if [[ $? != 0 ]]; then
-             echo  "FAILED: Docker Pull Failed on $IMAGES"
-             exit 1
-          fi
+    if [[ $? != 0 ]]; then
+      echo  "FAILED: Docker Pull Failed on $IMAGES"
+      exit 1
+    fi
+
     docker tag $ARTIFACTORY_URL/fabric-$IMAGES:$STABLE_TAG $ORG_NAME/fabric-$IMAGES
     docker tag $ARTIFACTORY_URL/fabric-$IMAGES:$STABLE_TAG $ORG_NAME/fabric-$IMAGES:$MASTER_TAG
     echo "$ORG_NAME/fabric-$IMAGES:$MASTER_TAG"
