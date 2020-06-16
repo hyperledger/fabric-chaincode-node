255325df9f32c2cb6e8c11052742a0f9f1e80ec0
Taku Shimosawa <taku.shimosawa@hal.hitachi.com>
2020-06-16 06:46:44 -0700

    [FABCN-412] TLS support for chaincode server (#164)
    This patch adds TLS support for chaincode server.
    
    To enable TLS, set tlsProps in the second argument for shim.server,
    or add --chaincode-tls-cert-file and --chaincode-tls.key-file for CLI.
    
    Client certificate validation can be enabled via tlsProps.clientCACerts
    for shim.server or --chaincode-tls-client-cacert-file for CLI.
    
    Also the -path options (for base64 encoded files) are supported.
    
    Signed-off-by: Taku Shimosawa <taku.shimosawa@hal.hitachi.com>

--- libraries/fabric-shim/lib/server.js
+++ libraries/fabric-shim/lib/server.js
@@ -47,10 +47,34 @@ class ChaincodeServer {
             throw new Error('The "chaincode" argument must implement Init() and Invoke() methods');
         }
         if (typeof serverOpts.ccid !== 'string') {
-            throw new Error('Missing required property in severOpts: ccid');
+            throw new Error('Missing required property in serverOpts: ccid');
         }
         if (typeof serverOpts.address !== 'string') {
-            throw new Error('Missing required property in severOpts: address');
+            throw new Error('Missing required property in serverOpts: address');
+        }
+        if (typeof serverOpts.tlsProps === 'object' && serverOpts.tlsProps !== null) {
+            if (typeof serverOpts.tlsProps.key !== 'object' || serverOpts.tlsProps.key === null) {
+                throw new Error('Missing required property in serverOpts.tlsProps: key');
+            }
+            if (typeof serverOpts.tlsProps.cert !== 'object' || serverOpts.tlsProps.cert === null) {
+                throw new Error('Missing required property in serverOpts.tlsProps: cert');
+            }
+
+            let clientCACerts;
+            if (typeof serverOpts.tlsProps.clientCACerts === 'object' && serverOpts.tlsProps.clientCACerts !== null) {
+                clientCACerts = serverOpts.tlsProps.clientCACerts;
+            } else {
+                clientCACerts = null;
+            }
+
+            this._credentials = grpc.ServerCredentials.createSsl(clientCACerts, [
+                {
+                    private_key: serverOpts.tlsProps.key,
+                    cert_chain: serverOpts.tlsProps.cert
+                }
+            ], clientCACerts === null ? false : true);
+        } else {
+            this._credentials = grpc.ServerCredentials.createInsecure();
         }
 
         // Create GRPC Server and register RPC handler
@@ -71,8 +95,7 @@ class ChaincodeServer {
         return new Promise((resolve, reject) => {
             logger.debug('ChaincodeServer trying to bind to ' + this._serverOpts.address);
 
-            // TODO: TLS Support
-            this._server.bindAsync(this._serverOpts.address, grpc.ServerCredentials.createInsecure(), (error, port) => {
+            this._server.bindAsync(this._serverOpts.address, this._credentials, (error, port) => {
                 if (!error) {
                     logger.debug('ChaincodeServer successfully bound to ' + port);
 
--- libraries/fabric-shim/test/unit/cmds/serverCommand.js
+++ libraries/fabric-shim/test/unit/cmds/serverCommand.js
@@ -10,6 +10,8 @@ const sinon = require('sinon');
 
 const chai = require('chai');
 const expect = chai.expect;
+const fs = require('fs');
+const path = require('path');
 
 const yargs = require('yargs');
 const Bootstrap = require('../../../lib/contract-spi/bootstrap');
@@ -30,6 +32,7 @@ describe('server cmd', () => {
         it('should configure the builder function', () => {
             sandbox.stub(yargs, 'options');
             sandbox.stub(yargs, 'usage');
+            sandbox.stub(yargs, 'check');
 
             chaincodeServerCommand.builder(yargs);
 
@@ -49,6 +52,7 @@ describe('server cmd', () => {
             expect(args['module-path'].default).to.deep.equal(process.cwd());
 
             expect(yargs.usage.calledOnce).to.be.true;
+            expect(yargs.check.calledOnce).to.be.true;
         });
     });
 
@@ -64,6 +68,16 @@ describe('server cmd', () => {
     });
 
     describe('.getArgs', () => {
+        const certFileEncoded = path.join(__dirname, '..', 'test-cert.base64');
+        const keyFileEncoded = path.join(__dirname, '..', 'test-key.base64');
+        const caFileEncoded = path.join(__dirname, '..', 'test-ca.base64');
+        const certFile = path.join(__dirname, '..', 'test-cert.pem');
+        const keyFile = path.join(__dirname, '..', 'test-key.pem');
+        const caFile = path.join(__dirname, '..', 'test-ca.pem');
+        const cert = Buffer.from(fs.readFileSync(certFileEncoded).toString(), 'base64');
+        const key = Buffer.from(fs.readFileSync(keyFileEncoded).toString(), 'base64');
+        const ca = Buffer.from(fs.readFileSync(caFileEncoded).toString(), 'base64');
+
         it('should return the arguments properly', () => {
             const argv = {
                 'chaincode-address': '0.0.0.0:9999',
@@ -82,6 +96,116 @@ describe('server cmd', () => {
             expect(ret['chaincode-address']).to.be.undefined;
             expect(ret['chaincode-id']).to.be.undefined;
             expect(ret['extra-options']).to.be.undefined;
+        });
+
+        it('should return the TLS arguments properly', () => {
+            const argv = {
+                'chaincode-address': '0.0.0.0:9999',
+                'chaincode-id': 'test_id:1',
+                'module-path': '/tmp/example',
+                'chaincode-tls-cert-path': certFileEncoded,
+                'chaincode-tls-key-path': keyFileEncoded
+            };
+
+            const ret = chaincodeServerCommand.getArgs({argv});
+
+            expect(ret.address).to.equal('0.0.0.0:9999');
+            expect(ret.ccid).to.equal('test_id:1');
+
+            expect(ret.tlsProps).to.deep.equal({
+                cert,
+                key
+            });
+        });
+
+        it('should return the mutual TLS arguments properly', () => {
+            const argv = {
+                'chaincode-address': '0.0.0.0:9999',
+                'chaincode-id': 'test_id:1',
+                'module-path': '/tmp/example',
+                'chaincode-tls-cert-path': certFileEncoded,
+                'chaincode-tls-key-path': keyFileEncoded,
+                'chaincode-tls-client-cacert-path': caFileEncoded
+            };
+
+            const ret = chaincodeServerCommand.getArgs({argv});
+
+            expect(ret.address).to.equal('0.0.0.0:9999');
+            expect(ret.ccid).to.equal('test_id:1');
+
+            expect(ret.tlsProps).to.deep.equal({
+                cert,
+                key,
+                clientCACerts: ca
+            });
+        });
+
+        it('should return the TLS arguments with PEM files properly', () => {
+            const argv = {
+                'chaincode-address': '0.0.0.0:9999',
+                'chaincode-id': 'test_id:1',
+                'module-path': '/tmp/example',
+                'chaincode-tls-cert-file': certFile,
+                'chaincode-tls-key-file': keyFile,
+                'chaincode-tls-client-cacert-file': caFile
+            };
+
+            const ret = chaincodeServerCommand.getArgs({argv});
+
+            expect(ret.address).to.equal('0.0.0.0:9999');
+            expect(ret.ccid).to.equal('test_id:1');
+
+            expect(ret.tlsProps).to.deep.equal({
+                cert,
+                key,
+                clientCACerts: ca
+            });
+        });
+    });
+
+    describe('parse arguments', () => {
+        it('should parse the arguments successfully', () => {
+            expect(() => {
+                chaincodeServerCommand.builder(yargs)
+                    .exitProcess(false)
+                    .parse('--chaincode-id test_id:1 --chaincode-address 0.0.0.0:9999');
+            }).not.to.throw();
+        });
+
+        it('should parse the arguments successfully with TLS options', () => {
+            expect(() => {
+                chaincodeServerCommand.builder(yargs)
+                    .exitProcess(false)
+                    .parse('--chaincode-id test_id:1 --chaincode-address 0.0.0.0:9999 ' +
+                        '--chaincode-tls-key-file tls.key --chaincode-tls-cert-file tls.pem');
+            }).not.to.throw();
+        });
+
+        it('should throw when conflicting arguments are passed', () => {
+            expect(() => {
+                chaincodeServerCommand.builder(yargs)
+                    .exitProcess(false)
+                    .parse('--chaincode-id test_id:1 --chaincode-address 0.0.0.0:9999 ' +
+                        '--chaincode-tls-key-file tls.key --chaincode-tls-key-path tls.pem');
+            }).to.throw();
+        });
+
+        it('should throw when only TLS key is passed', () => {
+            expect(() => {
+                chaincodeServerCommand.builder(yargs)
+                    .exitProcess(false)
+                    .parse('--chaincode-id test_id:1 --chaincode-address 0.0.0.0:9999 ' +
+                        '--chaincode-tls-key-file tls.key');
+            }).to.throw();
+        });
+
+        it('should throw when only TLS cert is passed', () => {
+            expect(() => {
+                chaincodeServerCommand.builder(yargs)
+                    .exitProcess(false)
+                    .parse('--chaincode-id test_id:1 --chaincode-address 0.0.0.0:9999 ' +
+                        '--chaincode-tls-cert-file tls.pem');
+            }).to.throw();
         });
     });
 });
