20c0d36cc391afec6b2ce864b4b71efbfa23640c
Matthew B White <whitemat@uk.ibm.com>
2021-03-02 11:30:03 +0000

    Simplify Naming
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

---
+++ ci/updatePackageJson.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let filename = process.argv[2];
+let input = JSON.parse(fs.readFileSync(filename));
+let ver = process.env.RELEASE_VERSION;
+input.version = ver;
+if (input.dependencies) {
+    if (input.dependencies['fabric-contract-api']) input.dependencies['fabric-contract-api'] = ver;
+    if (input.dependencies['fabric-ledger']) input.dependencies['fabric-ledger'] = ver;
+    if (input.dependencies['fabric-shim']) input.dependencies['fabric-shim'] = ver;
+    if (input.dependencies['fabric-shim-api']) input.dependencies['fabric-shim-api'] = ver;
+    if (input.dependencies['fabric-shim-crypto']) input.dependencies['fabric-shim-crypto'] = ver;
+}
+fs.writeFileSync(filename, JSON.stringify(input, null, 2));
