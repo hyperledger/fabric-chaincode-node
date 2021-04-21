8d4404c53253ebb107abafb5bd0e892d074bbb37
Matthew B White <whitemat@uk.ibm.com>
2021-04-21 13:44:18 +0100

    expr #2
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- docker/fabric-nodeenv/build.sh
+++ docker/fabric-nodeenv/build.sh
@@ -7,6 +7,13 @@ INPUT_DIR=/chaincode/input
 OUTPUT_DIR=/chaincode/output
 cp -R ${INPUT_DIR}/src/. ${OUTPUT_DIR}
 cd ${OUTPUT_DIR}
+
+ls -a ${OUTPUT_DIR}
+cat ${OUTPUT_DIR}/.npmrc
+
+ls -a ${INPUT_DIR}
+cat ${INPUT_DIR}/src/.npmrc
+
 if [ -f package-lock.json -o -f npm-shrinkwrap.json ]; then
     npm ci --only=production
 else
