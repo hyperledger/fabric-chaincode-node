f283df541ae1d52a7cf71c8abffcd1880e59462b
Matthew B White <whitemat@uk.ibm.com>
2021-04-21 14:16:38 +0100

    no --production?
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- docker/fabric-nodeenv/build.sh
+++ docker/fabric-nodeenv/build.sh
@@ -8,14 +8,10 @@ OUTPUT_DIR=/chaincode/output
 cp -R ${INPUT_DIR}/src/. ${OUTPUT_DIR}
 cd ${OUTPUT_DIR}
 
-ls -a ${OUTPUT_DIR}
 cat ${OUTPUT_DIR}/.npmrc
-
-ls -a ${INPUT_DIR}
-cat ${INPUT_DIR}/src/.npmrc
 
 if [ -f package-lock.json -o -f npm-shrinkwrap.json ]; then
     npm ci --only=production
 else
-    npm install --production
+    npm install
 fi
