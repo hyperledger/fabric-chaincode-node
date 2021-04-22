f695f857b94f93e7248c553470c4380a9db79611
Kestutis Gudynas <44440041+kemi04@users.noreply.github.com>
2021-04-22 15:00:38 +0100

    Pipeline publishing fixed
    Signed-off-by: Kestutis Gudynas <44440041+kemi04@users.noreply.github.com>

--- tools/toolchain/verdaccio/index.js
+++ tools/toolchain/verdaccio/index.js
@@ -10,6 +10,7 @@ const gulp = require('gulp');
 const { shell: runcmds } = require('../shell/cmd');
 const util = require('util');
 const path = require('path');
+const ip = require('ip');
 
 // Install from the dirs for use within the development context
 const installDir = (commands) =>{
@@ -24,8 +25,8 @@ const installDir = (commands) =>{
         const packageJSON = require(`../../../${npm_package.category}/${npm_package.name}/package.json`);
         const npm_tag = packageJSON.tag;
         const modulepath = path.resolve(`../../../${npm_package.category}/${npm_package.name}/`);
-        commands.push(util.format('npm publish --registry http://localhost:4873 %s --tag %s', modulepath, npm_tag));
-        commands.push(util.format('npm view --registry http://localhost:4873 %s', npm_package.name));
+        commands.push(util.format(`npm publish --registry http://${ip.address()}:4873 %s --tag %s`, modulepath, npm_tag));
+        commands.push(util.format(`npm view --registry http://${ip.address()}:4873 %s`, npm_package.name));
     }
 
     return commands;
@@ -44,8 +45,8 @@ const installTGZ = (commands) =>{
        const packageJSON = require(`../../../${npm_package.category}/${npm_package.name}/package.json`);
        const npm_tag = packageJSON.tag;
        const name = `${npm_package.name}-${packageJSON.version}.tgz`;
-       commands.push(util.format('npm publish --registry http://localhost:4873 ../../../build/%s --tag %s', name, npm_tag));
-       commands.push(util.format('npm view --registry http://localhost:4873 %s', npm_package.name));
+       commands.push(util.format(`npm publish --registry http://${ip.address()}:4873 ../../../build/%s --tag %s`, name, npm_tag));
+       commands.push(util.format(`npm view --registry http://${ip.address()}:4873 %s`, npm_package.name));
     }
 
     
@@ -58,8 +59,8 @@ const verdaccioStart = async () => {
         'docker rm -f verdaccio || true',
         util.format('docker run -d -p 4873:4873 -v %s/config.yaml:/verdaccio/conf/config.yaml --name verdaccio verdaccio/verdaccio', __dirname),
         'sleep 5', // verdaccio takes a while to start
-        'npm config delete //localhost:4873/:_authToken',
-        'npm-cli-login -u testuser -p testpass -e testuser@example.org -r http://localhost:4873',
+        `npm config delete //${ip.address()}:4873/:_authToken`,
+        `npm-cli-login -u testuser -p testpass -e testuser@example.org -r http://${ip.address()}:4873`,
         'sleep 5' // avoid "jwt not active" error
     ];
 
