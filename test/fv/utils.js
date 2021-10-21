95d0764227a6f2a56c50015cb136c73c93522c8a
Matthew B White <whitemat@uk.ibm.com>
2021-10-21 15:39:38 +0100

    Move to Fabric 2-4
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- test/fv/utils.js
+++ test/fv/utils.js
@@ -16,7 +16,7 @@ const execSync = require('child_process').execSync;
 const ordererCA = '/test-network/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem';
 const dir = path.join(__dirname, '..', '..', 'fabric-samples');
 const registerAndEnroll = () => {
-    const cmd = `export PATH=${dir}/bin:$PATH && export FABRIC_CFG_PATH=${dir}/config/ && ` +
+    const cmd = `export PATH="${dir}/bin:$PATH" && export FABRIC_CFG_PATH=${dir}/config/ && ` +
             `export FABRIC_CA_CLIENT_HOME=${dir}/test-network/organizations/peerOrganizations/org1.example.com/ && ` +
             `fabric-ca-client register --caname ca-org1 --id.name owner --id.secret ownerpw --id.type client --tls.certfiles "${dir}/test-network/organizations/fabric-ca/org1/tls-cert.pem" && ` +
             `fabric-ca-client enroll -u https://owner:ownerpw@localhost:7054 --caname ca-org1 -M "${dir}/test-network/organizations/peerOrganizations/org1.example.com/users/owner@org1.example.com/msp" --tls.certfiles "${dir}/test-network/organizations/fabric-ca/org1/tls-cert.pem" && ` +
@@ -52,7 +52,7 @@ async function install(ccName, transient) {
 async function instantiate(ccName, func, args) {
     let res = null;
     if (func !== undefined) {
-        const initCmd = `export PATH=${dir}/bin:$PATH && export FABRIC_CFG_PATH=${dir}/config/ && ` +
+        const initCmd = `export PATH="${dir}/bin:$PATH" && export FABRIC_CFG_PATH=${dir}/config/ && ` +
                 `export CORE_PEER_TLS_ENABLED=true && export CORE_PEER_LOCALMSPID="Org1MSP" && ` +
                 `export CORE_PEER_TLS_ROOTCERT_FILE=${dir}/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt && ` +
                 `export CORE_PEER_MSPCONFIGPATH=${dir}/test-network/organizations/peerOrganizations/org1.example.com/users/owner@org1.example.com/msp && ` +
@@ -77,7 +77,7 @@ function printArgs(func, args) {
 }
 
 async function invoke(ccName, func, args, transient) {
-    let cmd = `export PATH=${dir}/bin:$PATH && export FABRIC_CFG_PATH=${dir}/config/ && ` +
+    let cmd = `export PATH="${dir}/bin:$PATH" && export FABRIC_CFG_PATH=${dir}/config/ && ` +
         `export CORE_PEER_TLS_ENABLED=true && export CORE_PEER_LOCALMSPID="Org1MSP" && ` +
         `export CORE_PEER_TLS_ROOTCERT_FILE=${dir}/test-network/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt && ` +
         `export CORE_PEER_MSPCONFIGPATH=${dir}/test-network/organizations/peerOrganizations/org1.example.com/users/owner@org1.example.com/msp && ` +
@@ -95,7 +95,7 @@ async function invoke(ccName, func, args, transient) {
 }
 
 async function query(ccName, func, args, transient) {
-    let cmd = `export PATH=${dir}/bin:$PATH && export FABRIC_CFG_PATH=${dir}/config/ && ` +
+    let cmd = `export PATH="${dir}/bin:$PATH" && export FABRIC_CFG_PATH=${dir}/config/ && ` +
         `export CORE_PEER_TLS_ENABLED=true && export CORE_PEER_LOCALMSPID="Org2MSP" && ` +
         `export CORE_PEER_TLS_ROOTCERT_FILE=${dir}/test-network/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt && ` +
         `export CORE_PEER_MSPCONFIGPATH=${dir}/test-network/organizations/peerOrganizations/org2.example.com/users/buyer@org2.example.com/msp && ` +
@@ -120,7 +120,7 @@ async function installAndInstantiate(ccName, instantiateFunc, instantiateArgs, t
 }
 
 async function getLastBlock() {
-    const cmd = `export PATH=${dir}/bin:$PATH && export FABRIC_CFG_PATH=${dir}/config/ && ` +
+    const cmd = `export PATH="${dir}/bin:$PATH" && export FABRIC_CFG_PATH=${dir}/config/ && ` +
         `export CORE_PEER_TLS_ENABLED=true && export CORE_PEER_LOCALMSPID="Org2MSP" && ` +
         `export CORE_PEER_TLS_ROOTCERT_FILE=${dir}/test-network/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt && ` +
         `export CORE_PEER_MSPCONFIGPATH=${dir}/test-network/organizations/peerOrganizations/org2.example.com/users/buyer@org2.example.com/msp && ` +
