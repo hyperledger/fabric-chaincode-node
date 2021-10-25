4fd08d7958f1d7995bececf2e81108f35d625ccf
Matthew B White <whitemat@uk.ibm.com>
2021-10-25 15:35:00 +0100

    update promise handling
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- libraries/fabric-shim/cli.js
+++ libraries/fabric-shim/cli.js
@@ -10,24 +10,31 @@ const Logger = require('./lib/logger');
 
 const logger = Logger.getLogger('fabric-shim/cli');
 
-const results = require('yargs')
-    .parserConfiguration({'dot-notation':false})
-    .commandDir('./lib/cmds')
-    .demandCommand()
-    .help()
-    .wrap(null)
-    .alias('v', 'version')
-    .version(version)
-    .describe('v', 'show version information')
-    .env('CORE')
-    .argv;
 
-if (typeof(results.thePromise) !== 'undefined') {
-    results.thePromise.then(() => {
-    }).catch((error) => {
-        logger.info(error + '\nCommand failed\n');
-        process.exit(1);
-    });
-} else {
-    process.exit(0);
+const main = async () => {
+    const results = await require('yargs')
+        .parserConfiguration({ 'dot-notation': false })
+        .commandDir('./lib/cmds')
+        .demandCommand()
+        .help()
+        .wrap(null)
+        .alias('v', 'version')
+        .version(version)
+        .describe('v', 'show version information')
+        .env('CORE')
+        .argv;
+
+    if (typeof (results.thePromise) !== 'undefined') {
+        results.thePromise.then(() => {
+            logger.info("Bootstrap process completed")
+        }).catch((error) => {
+            logger.info(error + '\nCommand failed\n');
+            process.exit(1);
+        });
+    } else {
+        process.exit(0);
+    }
+
 }
+
+main();
