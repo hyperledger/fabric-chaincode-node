695828f10cafed313bf12311cb48f081f14f4cb1
Matthew B White <whitemat@uk.ibm.com>
2021-11-03 10:47:40 +0000

    Rename the root json config file
    Update the releasing notes
    
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

---
+++ tools/scripts/updateversions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Note uses bash4.4 or later features, and sponge from GNU moreutils
+set -eo pipefail
+
+if [ -z $1 ]; then
+    echo "Need to have the first arg set to the new package.json version "
+    exit 1
+fi
+
+NEW_VERSION=$1
+echo "Setting new version to '${NEW_VERSION}'"
+
+readarray -d '' PACKAGES < <(find . -name package.json -not -path '*/node_modules/*' -not -path '*/common/*')
+
+for PACKAGE in ${PACKAGES}
+do
+   echo "Updating '${PACKAGE}'"
+   jq --arg VER "${NEW_VERSION}" '.version=$VER' "${PACKAGE}" | sponge "${PACKAGE}"
+done
