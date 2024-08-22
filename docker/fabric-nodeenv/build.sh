08c02b4a06e9783de10fd92daa1d92ee35028e19
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2024-08-22 12:14:43 +0200

    Use Node 22 for chaincode Docker image
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- docker/fabric-nodeenv/build.sh
+++ docker/fabric-nodeenv/build.sh
@@ -7,8 +7,8 @@ INPUT_DIR=/chaincode/input
 OUTPUT_DIR=/chaincode/output
 cp -R ${INPUT_DIR}/src/. ${OUTPUT_DIR}
 cd ${OUTPUT_DIR}
-if [ -f package-lock.json -o -f npm-shrinkwrap.json ]; then
-    npm ci --only=production
+if [ -f package-lock.json ] || [ -f npm-shrinkwrap.json ]; then
+    npm ci --omit=dev
 else
-    npm install --production
+    npm install --omit=dev
 fi
